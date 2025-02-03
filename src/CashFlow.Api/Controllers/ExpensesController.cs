65d3e5b52625c1c81ba94fa57e85492afd2a3240
Jefferson Ferreira <Jefferson1489@gmail.com>
2025-02-03 17:59:35 -0300

    Introdução ao projeto

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Application.UseCase.Expenses.Register;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers
+{
+    public class ExpensesController : Controller
+    {
+
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+        {
+            var useCase = new RegisterExpenseUseCase();
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+    }
+}
---
+++ src/CashFlow.Application/UseCase/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,47 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCase.Expenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+        public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+        {
+            // TO DO VALIDATIONS
+
+            Validate(request);
+
+            return new ResponseRegisteredExpenseJson();
+        }
+
+        private void Validate(RequestRegisterExpenseJson request)
+        {
+            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+
+            if(titleIsEmpty)
+            {
+                throw new ArgumentException("The title is required");
+            }
+
+            if(request.Amount <= 0)
+            {
+                throw new ArgumentException("The amount must be than zero.");
+            }
+
+            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+            if(result > 0)
+            {
+                throw new ArgumentException("Expenses cannot be for the future");
+            }
+
+           var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+            if (paymentTypeIsValid == false)
+            {
+                throw new ArgumentException("Payment Type is not valid.");
+            }
+        }
+    }
+}
